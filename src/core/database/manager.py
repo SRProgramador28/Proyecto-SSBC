b7aec2598a46cefe8fc6c016e74ca5da687a3d39
Jose Luis <jose.becerra4633@alumnos.udg.mx>
2025-04-18 22:27:06 -0600

    Base de datos

---
+++ src/core/database/manager.py
@@ -0,0 +1,68 @@
+import mysql.connector
+from mysql.connector import Error
+
+class DatabaseManager:
+    def __init__(self, host, user, password, database, port):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+        self.port = port
+        self.connection = None
+
+    def init_schema(self):
+        try:
+            with open("src/core/database/schema.sql", "r") as file:
+                sql_commands = file.read().split(";")
+
+                for command in sql_commands:
+                    if command.strip():
+                        self.execute_query(command.strip())
+
+                print("Schema inicializado correctamente")
+        except Error as e:
+            print(f"Error al inicializar el esquema: {e}")
+        except FileNotFoundError:
+            print("El archivo schema.sql no se encontró")
+
+    def connect(self):
+        try:
+            self.connection = mysql.connector.connect(
+                host=self.host,
+                user=self.user,
+                password=self.password,
+                database=self.database
+            )
+            if self.connection.is_connected():
+                print("Conección exitosa")
+        except Error as e:
+            print(f"Error: {e}")
+            self.connection = None
+
+    def close(self):
+        if self.connection.is_connected():
+            self.connection.close()
+            print("Database connection closed")
+
+    def execute_query(self, query, params=None, fetch="all"):
+        if not self.connection or not self.connection.is_connected():
+            self.connect()
+
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute(query, params) if params else cursor.execute(query)
+
+            if query.strip().upper().startswith("SELECT"):
+                if fetch == "one":
+                    result = cursor.fetchone()
+                else:
+                    result = cursor.fetchall()
+                return result
+            else:
+                self.connection.commit()
+                return cursor.rowcount
+        except Error as e:
+            print(f"Error en consulta SQL: {e}")
+            return None
+        finally:
+            cursor.close()
