0d8d5995a6b2594ba082b8aceaefbf00a19c3820
Jose Luis <jose.becerra4633@alumnos.udg.mx>
2025-04-28 14:19:06 -0600

    Diagnostico, prolog

--- src/core/motor_inferencia/reglas/diagnostico.pl
+++ src/core/motor_inferencia/reglas/diagnostico.pl
@@ -1,1 +1,14 @@
 % Reglas de diagnostico
+diagnostico(Enfermedad) :-
+    enfermedad(Enfermedad),
+    tiene_sintomas(Enfermedad, ListaSintomas),
+    todos_sintomas_presentes(ListaSintomas).
+
+% Verifica si el paciente tiene los sintomas de la enfermedad
+todos_sintomas_presentes([]).
+todos_sintomas_presentes([Sintoma|Resto]) :-
+    sintoma(Sintoma),
+    todos_sintomas_presentes(Resto).
+
+
+
